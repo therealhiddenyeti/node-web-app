bda9c55d546de1c034a0829e36e0396c9bf60492
therealhiddenyeti <72407136+therealhiddenyeti@users.noreply.github.com>
2021-08-30 16:15:14 -0400

    added switch

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+//  res.send('Hello World');
+ res.statusCode = 200;
+ res.setHeader("Content-Type", "text/html");
+ switch (req.url) {
+   case "/home":
+     res.writeHead(200);
+     res.end("This is Home page<p>");
+     break;
+   case "/about":
+     res.writeHead(200);
+     res.end("This is About page<p>");
+     break;
+   default:     
+     res.writeHead(200);
+     res.end("This is Default page.  Other pages are /home and /about<p>");
+     break;
+ }
 });
 
 app.listen(PORT, HOST);
